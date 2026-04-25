9f8b6c51781477fa9c9ef3b527512f92cc2b65ab
Rohinth-Vijayaragunathan <rohinth2303@gmail.com>
2026-04-25 13:35:30 +0530

    requirements and runtime files added

--- employee_management_system/settings.py
+++ employee_management_system/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-do!a=934(*!kk5_ju4i$k*1v%*muu^2ikto@lt0s7pqqi68c@='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'employee_management_system.urls'
